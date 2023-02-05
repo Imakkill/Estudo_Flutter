b024e20a512f55338382fcd7bd18e36a00923ca4
Tallys <imakkamikill@gmail.com>
2023-02-05 19:31:58 -0300

    inicial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+const l = 10;
+void main() => runApp(
+      Column(
+        children: const <Widget>[
+          Text(
+            'Girls Girls Girls',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Something in the way',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Something in the way',
+            textDirection: TextDirection.ltr,
+          ),
+
+        ],
+      ),
+    );
